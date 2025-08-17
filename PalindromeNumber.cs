7b13a32268af7a552e3324d7b0fe4609077bd8a8
ricardosoares98 <ricardojorgess98@gmail.com>
2025-08-17 15:59:46 +0100

    some of leetcode solutions

---
+++ PalindromeNumber.cs
@@ -0,0 +1,27 @@
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+public class PalindromeNumber {
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) {
+            return false;
+        } else if (x >= 0 && x < 10) {
+            return true;
+        }
+        
+        var quantity = x.ToString().Length;
+
+        var charArray = x.ToString().ToCharArray();
+        
+        for (int i = 0; i < quantity; i++)
+        {
+            if (charArray[i] != charArray[quantity - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
